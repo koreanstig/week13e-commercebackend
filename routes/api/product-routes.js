1c0352a5a8726f8b84ae5e436b121a915fd011f2
Jung David Lee <jungdavidlee@gmail.com>
2021-03-25 11:46:44 -0700

    updated product.create and made product.destroy

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -63,8 +63,15 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
+  Product.create({
+
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    tagIds: req.body.tagIds,
+    category_id: req.body.category_id
+
+  }).then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -129,6 +136,19 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {id: req.params.id}
+  }).then(data => {
+    if (!data){
+      res.status(404).json({message: 'No product with this id is found!'});
+      return;
+    } else {
+      res.json(data);
+    }
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
