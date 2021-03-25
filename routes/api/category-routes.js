d7faef7fbe72051266e49f37191f0a080e4650b8
Jung David Lee <jungdavidlee@gmail.com>
2021-03-25 11:27:54 -0700

    added categroy

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -60,6 +60,19 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {id: req.params.id}
+  }).then(data => {
+    if (!data){
+      res.status(404).json({message: 'No category with this id found!'});
+      return;
+    } else {
+      res.json(data);
+    }
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
