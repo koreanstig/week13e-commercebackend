7f838f28f4973cf06e8e15f003ff1e593006a115
Jung David Lee <jungdavidlee@gmail.com>
2021-03-25 11:22:15 -0700

    added the findone category by its id value

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,6 +27,23 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {id: req.params.id},
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  }).then(data => {
+    if (!data) {
+      res.status(404).json({message: 'No categories found!'});
+      return;
+    } else {
+      res.json(data);
+    }
+  }).catch(err=>{
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
