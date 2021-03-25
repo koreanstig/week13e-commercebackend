c63f23b4f0ae121fa3ee445bfe05b0fdda917542
Jung David Lee <jungdavidlee@gmail.com>
2021-03-25 11:39:17 -0700

    added find one for products category

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -29,6 +29,28 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {id: req.params.id},
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [{
+      model: Category,
+      attributes: ['category_name']
+    },
+    {
+      model: Tag,
+      attributes: ['tag_name']
+    }]
+  }).then(data =>{
+    if(!data){
+      res.status(404).json({message: 'No product with this id is found!'});
+      return;
+    } else {
+      res.json(data);
+    }
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
